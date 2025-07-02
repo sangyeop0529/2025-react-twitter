18334a31a7f91112bfa2ac7dd2eda982d1de1646
DELL <sangyeop0529@gmail.com>
2025-07-02 17:41:29 +0900

    파이어베이스 auth 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import type firebase from "firebase/compat/app";
 
 export default defineConfig({
   plugins: [react()],
@@ -8,6 +9,7 @@ export default defineConfig({
     alias: {
       pages: path.resolve(__dirname, "./src/pages"),
       components: path.resolve(__dirname, "./src/components"),
+      firebaseApp: path.resolve(__dirname, "./src/firebaseApp"),
     },
   },
 });
